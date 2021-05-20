b741b4dd2ad6a9cbdeab12e214668a9a3ec7fab2
Traumtoter <aletorrez2001@gmail.com>
2021-05-19 19:32:47 -0600

    Commit para 0.3

---
+++ src/udp/Cliente.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udp;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+/**
+ *
+ * @author legion
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        final int PUERTO_SERVIDOR = 5000;
+        byte[] buffer = new byte[1024];
+        try{
+             InetAddress direccionServidor = InetAddress.getByName("10.0.2.15");
+             DatagramSocket socketUDP = new DatagramSocket();
+             Scanner teclado = new Scanner(System.in);
+             String mensaje;
+             System.out.println("INTRODUZCA UN MSJ PARA EL SERVIDOR (EXIT PARA SALIR)");
+             mensaje = teclado.nextLine();
+             buffer = mensaje.getBytes();
+             DatagramPacket pregunta = new DatagramPacket(buffer, buffer.length, direccionServidor, PUERTO_SERVIDOR);
+             System.out.println("El mensaje se esta enviando al servidor");
+             socketUDP.send(pregunta);
+             DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+             socketUDP.receive(peticion);
+           System.out.println("Mensaje recibido de SERVIDOR");
+            mensaje = new String(peticion.getData());
+            System.out.println(mensaje);
+            socketUDP.close();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SocketException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ src/udp/Servidor.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import java.io.*;
+import java.util.InputMismatchException;
+/**
+ *
+ * @author legion
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int PUERTO = 5000;
+        
+        byte[] buffer = new byte[1024];
+        try{
+           System.out.println("servidor conectado");
+           DatagramSocket socketUDP = new DatagramSocket(PUERTO);
+           //Ciclo para que siempre este recibiendo peticiones.
+           //while(true){
+               System.out.println("Servicio infinito");
+               int time = 10;
+                  DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+               socketUDP.receive(peticion);
+                 System.out.println("RECIBIENDO LA INFORMACION DESDE EL CLIENTE");
+                 String mensaje = new String(peticion.getData());
+                                    
+                 System.out.println(mensaje);
+                int puertoCliente = peticion.getPort();
+                InetAddress direccion = peticion.getAddress();
+                //mensaje = "¡Hola CLIENTE, soy el servidor!";
+                buffer = mensaje.getBytes();
+                DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, direccion, puertoCliente);
+                 System.out.println("La informacion se le ha enviado al cliente");
+                socketUDP.send(respuesta);
+           //}
+     
+        } catch (SocketException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
